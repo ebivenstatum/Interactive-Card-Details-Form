dc838a9976ba505071b4eb3ba390b4ffc0b88bd5
Elizabeth Bivens-Tatum <ebivenstatum@gmail.com>
2022-09-01 18:33:05 -0400

    Last of file setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import Form from './Form';
+
+function App() {
+
+  return (
+    <div>
+      <div className="border"></div>
+      <Form />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/Form.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+function Form() {
+  return(
+    <div className="input"></div>
+  )
+}
+
+export default Form;
