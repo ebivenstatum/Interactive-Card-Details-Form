8f839101de89ae6a9a555f89ab87c982e08dd0c8
Elizabeth Bivens-Tatum <ebivenstatum@gmail.com>
2022-10-09 22:33:24 -0400

    Added server for static file and started form

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -1,8 +1,46 @@
 import React, { useState, useEffect } from 'react';
 
 function Form() {
+
+  const [cardholderName, setCardholderName] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
+  const [cvc, setCVC] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  const handleCardholderNameChange = (event) => {}
+  const handleCardNumberChange = (event) => {}
+  const handleMonthChange = (event) => {}
+  const handleYearChange = (event) => {}
+  const handleCVCChange = (event) => {}
+
   return(
-    <div className="input"></div>
+    <div className="input">
+      <form onSubmit={handleSubmit}>
+        <label>
+          CARDHOLDER NAME /n
+          <input type="text" className="fullInput" name="cardholderName" value={cardholderName} onChange={handleCardholderNameChange}></input>
+        </label>
+        <label>
+          CARD NUMBER /n
+          <input type="text" className="fullInput" name="cardNumber" maxLength={16} value={cardNumber} onChange={handleCardNumberChange}></input>
+        </label>
+        <label>
+          EXP. DATE (MM/YY) /n
+          <input type="text" className="quarterInput" name="month" maxLength={2} value={month} onChange={handleMonthChange}></input>
+          <input type="text" className="quarterInput" name="year" maxLength={2} value={year} onChange={handleYearChange}></input>
+        </label>
+        <label>
+          CVC /n
+          <input type="text" className="halfInput" name="cvc" maxLength={3} value={cvc} onChange={handleCVCChange}></input>
+        </label>
+        <input type="submit" className="fullInput" name="comfirm" value="Confirm"></input>
+      </form>
+    </div>
   )
 }
 
